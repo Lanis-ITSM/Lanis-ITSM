4fd9299f2c34666df867caec1bf05832d5aab5d7
sandikodev <sandikodev@users.noreply.github.com>
2023-02-01 02:05:57 +0000

    Deploying to gh-pages from @ develapar/develapar.github.io@8a34a4f27db79977d35dd5f567976032d4768111 🚀

---
+++ _app/immutable/chunks/4-b3cb1dc6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/bootstrap/_page.svelte-b8c6c798.js";export{t as component};
---
+++ _app/immutable/chunks/5-0e768bbd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/coba/_page.svelte-a0b272f9.js";export{t as component};
